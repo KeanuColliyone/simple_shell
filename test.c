589c11bd094f6625f45d21051f41547c97faa779
Kenan Kotzee <frostcolliyone@gmail.com>
2023-11-15 15:55:34 -0800

    aaahhhhhh

--- test.c
+++ test.c
@@ -4,85 +4,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <limits.h>
 
 extern char **environ;
 
 #define BUFFER_SIZE 1024
-
-char **tokenize_input(char *input) {
-
-	int i;
-	char *token;
-
-    char **tokens = (char **)malloc(BUFFER_SIZE * sizeof(char *));
-    if (tokens == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    i = 0;
-    token = strtok(input, " \t\n");
-    while (token != NULL) {
-        tokens[i++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    tokens[i] = NULL;
-
-    return tokens;
-}
-
-char *custom_getline(void) {
-    static char buffer[BUFFER_SIZE];
-    static int buffer_index = 0;
-    static int chars_read = 0;
-
-    char *line = NULL;
-    int index = 0;
-    int newline_found = 0;
-    int copy_length;
-    int i;
-
-    while (!newline_found) {
-        if (buffer_index >= chars_read) {
-            chars_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            buffer_index = 0;
-
-            if (chars_read <= 0) {
-                if (line != NULL && index > 0) {
-                    line[index] = '\0';
-                    return line;
-                }
-                return NULL;
-            }
-        }
-
-        while (buffer_index < chars_read) {
-            if (buffer[buffer_index] == '\n') {
-                newline_found = 1;
-                break;
-            }
-            buffer_index++;
-        }
-
-        copy_length = buffer_index - index;
-        if (copy_length > 0) {
-            line = (char *)realloc(line, (index + copy_length + 1) * sizeof(char));
-
-            for (i = 0; i < copy_length; i++) {
-                line[index++] = buffer[i];
-            }
-        }
-
-        if (newline_found) {
-            buffer_index++;
-            line[index++] = '\n';
-            line[index] = '\0';
-            return line;
-        }
-    }
-
-    return NULL;
-}
 
 void execute_command(char *args[]) {
     pid_t child_pid;
@@ -108,14 +35,18 @@ void execute_command(char *args[]) {
 void print_environment();
 
 int main(void) {
-    char *buffer;
+    char buffer[BUFFER_SIZE];
     size_t n;
+    char *args[BUFFER_SIZE];
+    int i;
     int found;
     char *path;
     char *path_copy;
     char *dir;
     char cmd_path[BUFFER_SIZE];
-    char **args;
+    char *token;
+    char *semi_pos;
+    char *temp;
 
     int is_interactive = isatty(STDIN_FILENO);
 
@@ -124,7 +55,7 @@ int main(void) {
             printf("$ ");
         }
         
-        if ((buffer = custom_getline()) == NULL) {
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
             printf("\n");
             break;
         }
@@ -134,14 +65,30 @@ int main(void) {
             buffer[n - 1] = '\0';
         }
 
-	args = tokenize_input(buffer);
+        token = buffer;
 
-        if (strcmp(args[0], "exit") == 0) {
-            printf("Exiting the shell\n");
-            break;
-        } else if (strcmp(args[0], "env") == 0) {
-            print_environment();
-        } else {
+        while ((semi_pos = strchr(token, ';')) != NULL) {
+            *semi_pos = '\0';
+            i = 0;
+            temp = token;
+            while (*temp != '\0') {
+                while (*temp == ' ') {
+                    temp++;
+                }
+                if (*temp == '\0') {
+                    break;
+                }
+                args[i++] = temp;
+                while (*temp != ' ' && *temp != '\0') {
+                    temp++;
+                }
+                if (*temp == '\0') {
+                    break;
+                }
+                *temp++ = '\0';
+            }
+            args[i] = NULL;
+
             found = 0;
 
             if (strchr(args[0], '/') != NULL) {
@@ -172,14 +119,64 @@ int main(void) {
             if (!found) {
                 printf("Command not found: %s\n", args[0]);
             }
+
+            token = semi_pos + 1;
+        }
+
+        i = 0;
+        temp = token;
+        while (*temp != '\0') {
+            while (*temp == ' ') {
+                temp++;
+            }
+            if (*temp == '\0') {
+                break;
+            }
+            args[i++] = temp;
+            while (*temp != ' ' && *temp != '\0') {
+                temp++;
+            }
+            if (*temp == '\0') {
+                break;
+            }
+            *temp++ = '\0';
+        }
+        args[i] = NULL;
+
+        found = 0;
+
+        if (strchr(args[0], '/') != NULL) {
+            if (access(args[0], X_OK) == 0) {
+                found = 1;
+                execute_command(args);
+            }
+        } else {
+            path = getenv("PATH");
+            path_copy = strdup(path);
+            dir = strtok(path_copy, ":");
+
+            while (dir != NULL) {
+                snprintf(cmd_path, BUFFER_SIZE, "%s/%s", dir, args[0]);
+
+                if (access(cmd_path, X_OK) == 0) {
+                    found = 1;
+                    execute_command(args);
+                    break;
+                }
+
+                dir = strtok(NULL, ":");
+            }
+
+            free(path_copy);
+        }
+
+        if (!found) {
+            printf("Command not found: %s\n", args[0]);
         }
 
         if (!is_interactive) {
             break;
         }
-
-	free(buffer);
-	free(args);
     }
 
     return EXIT_SUCCESS;
@@ -192,4 +189,3 @@ void print_environment() {
         env++;
     }
 }
-
